4631216c2777192c3e6cec954a2885cee07d36d4
William Ferreira <wqferr@gmail.com>
2019-07-23 00:40:11 -0300

    Fix init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- Helper Lua file for easy require if tiny-ecs is used as a git submodule or
 -- folder. Not needed in many cases, including luarocks distribution.
 
-local directory = (...):match("(.-)[^%.]+$")
-return require(directory .. 'tiny')
+local args = {...}
+local directory = args[1]
+return require(directory .. '.tiny')
